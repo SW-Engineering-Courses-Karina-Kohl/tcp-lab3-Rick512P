d9e272d2c0ff1a13432fa9759ef0c345b8da8a3c
Érick <98136250+Rick512P@users.noreply.github.com>
2025-09-15 15:08:58 -0300

    Criação Classe FiguraGeometrica
    Realizada criação de classe
    Adicionado construtor padrão para ID, contador, cor
    Adicionados gets e sets básicos

---
+++ src/FiguraGeometrica.java
@@ -0,0 +1,24 @@
+public abstract class FiguraGeometrica {
+    private String Cor;
+    private String ID;
+    private static int TotalFiguras = 0;
+    public FiguraGeometrica(String Cor) {
+        this.Cor = Cor;
+        this.ID = String.format("F%05d", TotalFiguras);
+        TotalFiguras++;
+    }
+    public String getCor() {
+        return Cor;
+    }
+    public void setCor(String Cor) {
+        this.Cor = Cor;
+    }
+
+    public static int getTotalFiguras() {
+        return TotalFiguras;
+    }
+    public String getInfo() {
+        return "Cor: " + Cor + ", ID: " + ID;
+    }
+    public abstract String GetDetales();
+}
